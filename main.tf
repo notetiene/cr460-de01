db4adf3ed9840331a61584abe1558674c74cb0d3
Etienne Prud’homme <e.e.f.prudhomme@gmail.com>
2024-02-26 18:03:02 -0500

    Ajout de réseau virtuel Azure
    * main.tf: Add cr460-de01-network virtual network.

--- main.tf
+++ main.tf
@@ -28,3 +28,21 @@ resource "azurerm_resource_group" "cr460-de01" {
   name     = "cr460-de01-resources"
   location = "Canada East"
 }
+
+resource "azurerm_virtual_network" "cr460-de01" {
+  name                = "cr460-de01-network"
+  location            = azurerm_resource_group.cr460-de01.location
+  resource_group_name = azurerm_resource_group.cr460-de01.name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["1.1.1.1", "1.0.0.1"]
+
+  subnet {
+    # P = #x50
+    name           = "PolyMTL"
+    address_prefix = "10.0.120.0/24"
+  }
+
+  tags = {
+    environment = "cr460"
+  }
+}
